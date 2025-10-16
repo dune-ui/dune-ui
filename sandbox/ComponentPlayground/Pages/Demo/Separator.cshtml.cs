5d7e62f467b927ddea8cfc12f266ec0b41de261f
Jerrie Pelser <jerrie@jerriepelser.com>
2025-10-16 20:55:29 +0700

    Adds separator and spinner demo pages

---
+++ sandbox/ComponentPlayground/Pages/Demo/Separator.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ComponentPlayground.Pages.Demo;
+
+public class Separator : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
---
+++ sandbox/ComponentPlayground/Pages/Demo/Spinner.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ComponentPlayground.Pages.Demo;
+
+public class Spinner : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
