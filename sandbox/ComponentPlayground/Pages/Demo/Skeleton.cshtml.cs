03d2798b768586cf16e46fc9a1467a3f203dd715
Jerrie Pelser <jerrie@jerriepelser.com>
2025-10-17 07:59:46 +0700

    Adds skeleton

---
+++ sandbox/ComponentPlayground/Pages/Demo/Skeleton.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ComponentPlayground.Pages.Demo;
+
+public class Skeleton : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
---
+++ src/StellarUI/TagHelpers/Skeleton/SkeletonTagHelper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace StellarUI.TagHelpers;
+
+[HtmlTargetElement("sui-skeleton")]
+public class SkeletonTagHelper(ICssClassMerger classMerger) : StellarTagHelper
+{
+    public override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+    {
+        output.TagName = "div";
+        output.TagMode = TagMode.StartTagAndEndTag;
+
+        output.Attributes.SetAttribute("data-slot", "skeleton");
+        output.Attributes.SetAttribute(
+            "class",
+            classMerger.Merge("bg-accent animate-pulse rounded-md", output.GetUserSuppliedClass())
+        );
+
+        return Task.CompletedTask;
+    }
+}
