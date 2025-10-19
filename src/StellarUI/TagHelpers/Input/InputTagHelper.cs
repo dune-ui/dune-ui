4a44f5f85ace23abef3ac77807bb5d4744abad6b
Jerrie Pelser <jerrie@jerriepelser.com>
2025-10-19 14:17:25 +0700

    Adds input

---
+++ src/StellarUI/TagHelpers/Input/InputTagHelper.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace StellarUI.TagHelpers;
+
+[HtmlTargetElement("sui-input", TagStructure = TagStructure.WithoutEndTag)]
+public class InputTagHelper(ICssClassMerger classMerger) : StellarTagHelper
+{
+    public override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+    {
+        output.TagName = "input";
+        output.TagMode = TagMode.SelfClosing;
+
+        output.Attributes.SetAttribute("data-slot", "input");
+        output.Attributes.SetAttribute(
+            "class",
+            classMerger.Merge(
+                "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+                "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
+                "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+                output.GetUserSuppliedClass()
+            )
+        );
+
+        return Task.CompletedTask;
+    }
+}
