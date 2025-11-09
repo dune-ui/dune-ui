205858b643ac575494d278f74faf24c52f233a2b
Jerrie Pelser <jerrie@jerriepelser.com>
2025-11-09 15:28:34 +0700

    Adds empty docs

---
+++ docs/DocsSamples/Pages/Empty/Avatar.cshtml.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DocsSamples.Pages.Empty;
+
+public class Avatar : PageModel
+{
+    public void OnGet() { }
+}
---
+++ docs/DocsSamples/Pages/Empty/Intro.cshtml.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DocsSamples.Pages.Empty;
+
+public class Intro : PageModel
+{
+    public void OnGet() { }
+}
