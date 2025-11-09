c60d42d1296a487a0cedd5425e705ccc68ff3b88
Jerrie Pelser <jerrie@jerriepelser.com>
2025-11-09 17:15:22 +0700

    Adds icon docs

---
+++ docs/DocsSamples/Pages/Icon/Color.cshtml.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DocsSamples.Pages.Icon;
+
+public class Color : PageModel
+{
+    public void OnGet() { }
+}
---
+++ docs/DocsSamples/Pages/Icon/StrokeWidth.cshtml.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DocsSamples.Pages.Icon;
+
+public class StrokeWidth : PageModel
+{
+    public void OnGet() { }
+}
