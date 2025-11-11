e049d0227a2d7d21dbedc51d31ccce0a8f2ce597
Jerrie Pelser <jerrie@jerriepelser.com>
2025-11-11 20:26:39 +0700

    Adds spinner docs

---
+++ docs/DocsSamples/Pages/Spinner/Color.cshtml.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DocsSamples.Pages.Spinner;
+
+public class Color : PageModel
+{
+    public void OnGet() { }
+}
---
+++ docs/DocsSamples/Pages/Spinner/Size.cshtml.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DocsSamples.Pages.Spinner;
+
+public class Size : PageModel
+{
+    public void OnGet() { }
+}
