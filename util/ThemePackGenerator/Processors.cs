c69a2ea363f533320049a425589e17d60ff56a86
Jerrie Pelser <jerrie@jerriepelser.com>
2025-12-28 17:04:13 +0700

    Adds Themepack generator

---
+++ util/ThemePackGenerator/Processors.cs
@@ -0,0 +1,117 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ThemePackGenerator;
+
+public static partial class Processors
+{
+    [GeneratedRegex(@"\saria-invalid:ring-(\[)?[1-9](px\])?")]
+    public static partial Regex AriaInvalidRingRegex();
+
+    [GeneratedRegex(@"(\s)(?<dark>dark:)?(aria\-invalid:)(?<class>\S+)")]
+    public static partial Regex AriaInvalidClassRegex();
+
+    [GeneratedRegex(@"(data-\[slot=checkbox-group\]:)(?<class>\S+)")]
+    public static partial Regex FieldGroupCheckboxGroupRegex();
+
+    extension(Dictionary<string, string> input)
+    {
+        /// <summary>
+        ///     dui-field-group contains a rule for data-slot=checkbox-group which tightens the gap between checkboxes.
+        ///     We want to duplicate this rule for data-slot=radio-group so radio group spacing is also tightened up.
+        /// </summary>
+        public Dictionary<string, string> AddFieldRadioGroupSupport()
+        {
+            var output = new Dictionary<string, string>(input);
+
+            if (output.TryGetValue("dui-field-group", out var fieldGroupClass))
+            {
+                foreach (Match match in FieldGroupCheckboxGroupRegex().Matches(fieldGroupClass))
+                {
+                    fieldGroupClass += $" data-[slot=radio-group]:{match.Groups["class"].Value}";
+                }
+
+                // Store the updated
+                output["dui-field-group"] = fieldGroupClass;
+            }
+
+            return output;
+        }
+
+        /// <summary>
+        ///     Remove the ring around inputs that are in an error state. The ring is confusing as it is the same
+        ///     ring used when an input has focus, so the presence of this ring makes it difficult to see when an
+        ///     input that is in error state has input focus
+        /// </summary>
+        public Dictionary<string, string> RemoveAriaInvalidRing()
+        {
+            var output = new Dictionary<string, string>();
+
+            foreach (var (key, value) in input)
+            {
+                var newValue = value;
+                
+                foreach (Match match in AriaInvalidClassRegex().Matches(value))
+                {
+                    newValue += $" {match.Groups["dark"].Value}[&.input-validation-error]:{match.Groups["class"].Value}";
+                }
+                
+                output.Add(key, newValue);
+            }
+
+            return output;
+        }
+
+        /// <summary>
+        /// Shadcn using the aria-invalid attribute to indicate errors. We want to make copies of all those classes
+        /// and create a version that depends on whether the .input-validation-error class is added (which is what
+        /// is added by ASP.NET Core validation).
+        /// </summary>
+        /// <returns></returns>
+        public Dictionary<string, string> CreateInputValidationErrorClassesFromAriaInvalid()
+        {
+            var output = new Dictionary<string, string>();
+
+            foreach (var (key, value) in input)
+            {
+                output.Add(key, AriaInvalidRingRegex().Replace(value, string.Empty));
+            }
+
+            return output;
+        }
+
+        /// <summary>
+        /// dui-checkbox uses the data-checked attribute to style the checked state since that is what is
+        /// being used by BaseUI. but since we use the native checkbox we should use the standard checked
+        /// pseudo class instead. 
+        /// </summary>
+        /// <returns></returns>
+        public Dictionary<string, string> ReplaceDuiCheckboxDataChecked()
+        {
+            var output = new Dictionary<string, string>(input);
+
+            if (output.TryGetValue("dui-checkbox", out var classes))
+            {
+                output["dui-checkbox"] = classes.Replace("data-checked:", "checked:");
+            }
+
+            return output;
+        }
+        /// <summary>
+        /// dui-checkbox uses the data-checked attribute to style the checked state since that is what is
+        /// being used by BaseUI. but since we use the native checkbox we should use the standard checked
+        /// pseudo class instead. 
+        /// </summary>
+        /// <returns></returns>
+        public Dictionary<string, string> ReplaceDuiRadioGroupItemDataChecked()
+        {
+            var output = new Dictionary<string, string>(input);
+
+            if (output.TryGetValue("dui-radio-group-item", out var classes))
+            {
+                output["dui-radio-group-item"] = classes.Replace("data-checked:", "checked:");
+            }
+
+            return output;
+        }
+    }
+}
---
+++ util/ThemePackGenerator/Program.cs
@@ -0,0 +1,71 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.RegularExpressions;
+
+namespace ThemePackGenerator;
+
+public partial class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var themePackFolder = @"C:\development\dune-ui\dune-ui\src\DuneUI.Core\Theming\ThemePacks";
+        string[] themeFiles =
+        [
+            "https://raw.githubusercontent.com/shadcn-ui/ui/refs/heads/main/apps/v4/registry/styles/style-lyra.css",
+            "https://raw.githubusercontent.com/shadcn-ui/ui/refs/heads/main/apps/v4/registry/styles/style-maia.css",
+            "https://raw.githubusercontent.com/shadcn-ui/ui/refs/heads/main/apps/v4/registry/styles/style-mira.css",
+            "https://raw.githubusercontent.com/shadcn-ui/ui/refs/heads/main/apps/v4/registry/styles/style-nova.css",
+            "https://raw.githubusercontent.com/shadcn-ui/ui/refs/heads/main/apps/v4/registry/styles/style-vega.css",
+        ];
+
+        var httpClient = new HttpClient();
+
+        foreach (var themeFile in themeFiles)
+        {
+            var responseMessage = await httpClient.GetAsync(themeFile);
+
+            responseMessage.EnsureSuccessStatusCode();
+
+            var responseText = await responseMessage.Content.ReadAsStringAsync();
+            var themeStyles = ExtractComponentsFromThemeStyle(responseText)
+                .AddFieldRadioGroupSupport()
+                .RemoveAriaInvalidRing()
+                .CreateInputValidationErrorClassesFromAriaInvalid()
+                .ReplaceDuiCheckboxDataChecked()
+                .ReplaceDuiRadioGroupItemDataChecked();
+
+            var fileName = Path.GetFileNameWithoutExtension(themeFile)
+                .Replace("style-", string.Empty);
+
+            var themePackText = string.Join(
+                Environment.NewLine,
+                themeStyles.Select(pair =>
+                    $"-{pair.Key}{Environment.NewLine}{pair.Value}{Environment.NewLine}"
+                )
+            );
+
+            await File.WriteAllTextAsync(
+                Path.Combine(
+                    themePackFolder,
+                    char.ToUpperInvariant(fileName[0]) + fileName[1..] + ".themepack"
+                ),
+                themePackText
+            );
+        }
+    }
+
+    [GeneratedRegex(@".cn-(?<name>\S+).*{\n(\s)*@apply(\s)+(?<value>.*);")]
+    public static partial Regex StyleRegex();
+
+    private static Dictionary<string, string> ExtractComponentsFromThemeStyle(string input)
+    {
+        var componentStyles = new Dictionary<string, string>();
+
+        foreach (Match match in StyleRegex().Matches(input))
+        {
+            componentStyles.Add($"dui-{match.Groups["name"].Value}", match.Groups["value"].Value);
+        }
+
+        return componentStyles;
+    }
+}
