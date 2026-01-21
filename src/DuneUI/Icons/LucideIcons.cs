2122ca07dedefac955d2d908bea42a907ec0b93d
Jerrie Pelser <jerrie@jerriepelser.com>
2026-01-21 20:51:02 +0700

    Split icon definitions to try and improve .NET 10 compilation speed

--- src/DuneUI/Icons/LucideIcons.cs
+++ src/DuneUI/Icons/LucideIcons.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections.Frozen;
-using System.Collections.Immutable;
-
-namespace DuneUI.Icons;
+﻿namespace DuneUI.Icons;
 
 internal static partial class LucideIcons
 {
@@ -20,35 +17,4 @@ internal static partial class LucideIcons
         ["stroke-linecap"] = "round",
         ["stroke-linejoin"] = "round",
     };
-
-    public static List<SvgShape> xIconAArrowDown =
-    [
-        new SvgShape(
-            "path",
-            new Dictionary<string, string> { ["d"] = "m14 12 4 4 4-4" }.ToImmutableDictionary()
-        ),
-        new SvgShape(
-            "path",
-            new Dictionary<string, string> { ["d"] = "M18 16V7" }.ToImmutableDictionary()
-        ),
-        new SvgShape(
-            "path",
-            new Dictionary<string, string>
-            {
-                ["d"] = "m2 16 4.039-9.69a.5.5 0 0 1 .923 0L11 16",
-            }.ToImmutableDictionary()
-        ),
-        new SvgShape(
-            "path",
-            new Dictionary<string, string> { ["d"] = "M3.304 13h6.392" }.ToImmutableDictionary()
-        ),
-    ];
-
-    public static FrozenDictionary<string, IconDefinition> xIconDefinitions = new Dictionary<
-        string,
-        IconDefinition
-    >
-    {
-        ["xxx"] = new IconDefinition(SvgAttributes.ToImmutableDictionary(), xIconAArrowDown),
-    }.ToFrozenDictionary();
 }
