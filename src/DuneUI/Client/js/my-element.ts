8505718d0b3a62bb134ff5b8ec2640269ffcf897
Jerrie Pelser <jerrie@jerriepelser.com>
2026-02-26 08:43:43 +0700

    Configures initial build pipeline for Lit components

---
+++ src/DuneUI/Client/js/my-element.ts
@@ -0,0 +1,15 @@
+﻿import { LitElement, html, css } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
+
+@customElement('my-element')
+export class MyElement extends LitElement {
+    static styles = css`
+    :host { color: blue; font-family: sans-serif; }
+  `;
+
+    @property() name = 'World';
+
+    render() {
+        return html`<h1>Hello, ${this.name}!</h1>`;
+    }
+}
