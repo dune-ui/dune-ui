7adf1b2b0bb19e2905cce802824582743307a8b0
Jerrie Pelser <jerrie@jerriepelser.com>
2026-03-31 08:23:35 +0700

    Adds dialog() helper

--- src/DuneUI/Client/js/dune-ui.ts
+++ src/DuneUI/Client/js/dune-ui.ts
@@ -1,3 +1,19 @@
-﻿import "./my-element";
-import "./del-collapsible";
-import "./del-dialog";
+﻿import "./web-components/del-collapsible";
+import "./web-components/del-dialog";
+
+import { dialog } from "./wrappers/dialog";
+
+const duneui = { dialog };
+
+// augment the Window type so TS knows about window.duneui
+declare global {
+  interface Window {
+    duneui: typeof duneui;
+  }
+}
+
+if (window.duneui) {
+  console.warn("duneui: window.duneui is already defined and will be overwritten.");
+}
+
+window.duneui = duneui;
